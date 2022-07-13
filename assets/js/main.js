6505fb985f795e70cb072336ba03a526629479e1
João Paulo Menezes <joaopaulo.smenezes@hotmail.com>
2022-07-13 11:47:29 -0300

    css and js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.querySelector('#header');    
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+                       : header.classList.remove('scroll-header');    
+}
+window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== SWIPER POPULAR ===============*/

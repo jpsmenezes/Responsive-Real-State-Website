55de1c8e2b296a22c24f6ff1400ed883ff0fcc93
João Paulo Menezes <joaopaulo.smenezes@hotmail.com>
2022-07-19 12:21:38 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,22 @@ const swiperPopular = new Swiper(".popular__container", {
 });
 
 /*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value__accordion-item');
 
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector('.value__accordion-header');
+
+  accordionHeader.addEventListener('click', ()=>{
+    toggleItem(item);
+  });
+});
+
+const toggleItem = (item) =>{
+  const accordionContent = item.querySelector('.value__accordion-content');
+
+  accordionContent.style.height = accordionContent.scrollHeight + 'px';
+  item.classList.add('accordion-open');
+}
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 

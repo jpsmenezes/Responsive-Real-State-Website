dda32bf86ad3373fdd4a4dd764d2ebd44dc25605
João Paulo Menezes <joaopaulo.smenezes@hotmail.com>
2022-08-03 11:02:38 -0300

    aaa

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,6 +76,13 @@ window.addEventListener('scroll', scrollActive);
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.querySelector('#scroll-up');
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						          : scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
 
 
 /*=============== DARK LIGHT THEME ===============*/ 
